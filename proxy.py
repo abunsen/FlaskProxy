5bed14b391106897c328308b83830ba50f6d7b07
Auston Bunsen <auston.bunsen@gmail.com>
2016-04-23 18:30:35 -0400

    testing

--- proxy.py
+++ proxy.py
@@ -12,7 +12,7 @@ import urlparse
 
 app = Flask(__name__)
 # app.config['SERVER_NAME'] = 'userpathpreviews.com'
-# app.debug = True
+app.debug = True
 
 needs_parsed = [
     'text/html',
@@ -96,5 +96,5 @@ def hello(url):
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(port=5000)
+    app.run(port=8080)
     # app.run(host=app.config['SERVER_NAME'], port=port)
